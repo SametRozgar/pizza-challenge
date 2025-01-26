cfc233a73bb6070e82821c827ce3f957e7d205b9
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-01-27 01:33:14 +0300

    slider ve header eklendi router yapısı düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
-
-import Pizzalar from "./components/Pizzalar"
-import SiparisFormu from "./components/SiparisFormu"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Navbar from "./components/navbar";
+import Pizzalar from "./components/Pizzalar";
+import SiparisFormu from "./components/SiparisFormu";
+import Slider from "./components/Slider";
+import Menu from "./components/Menu"
 function App() {
-  
-
   return (
-<Router>
-      <Switch>
-        <Route exact path="/" component={Pizzalar} />
-        <Route  path="/siparisformu" component={SiparisFormu} />
-      </Switch>
-    </Router>
-  )
+    <div>
+      <Router>
+        <Navbar />
+
+       
+        <Switch>
+          <Route exact path="/">
+            <Slider />
+            <Menu/>
+            <Pizzalar />
+          </Route>
+          
+          <Route path="/siparisformu" component={SiparisFormu} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
